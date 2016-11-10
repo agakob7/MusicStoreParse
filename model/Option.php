ed3734bae9a39a0bd7cf994e9c314c6f47f44130
agakob7 <arachnia@o2.pl>
2016-11-10 15:28:55 +0100

    Change

---
+++ model/Option.php
@@ -0,0 +1,18 @@
+<?php
+
+class Option
+{
+
+    public $parse_limit = 20;
+    public $weight;
+
+    public $width;
+    public $depth;
+    public $height;
+    public $name_suffix;
+
+    public $categories;
+    public $producer;
+
+
+}
